afa7229257e39a527dee8ffb90fe38a7a82dcc25
anash24 <anash24@penguinhall.org>
2022-10-24 11:09:40 -0400

    js refresher

---
+++ refresher.js
@@ -0,0 +1,17 @@
+// console.log('Hello world!');
+ let message = 'How are you today?';
+// console.log(message);
+
+let greeting = 'Hello';
+let name = 'Bob';
+let text3 = greeting + ' ' + name + '!';
+// console.log(text3);
+
+let age = 13;
+// console.log('You are' + ' ' + age);
+let years = 10 + age;
+// console.log('In ten years you will be' + ' ' + years);
+
+// Part 2
+
+
