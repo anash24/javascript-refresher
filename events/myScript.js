3d3c2e67ddfce56cabc44051f1a4467705a547da
anash24 <anash24@penguinhall.org>
2022-11-18 08:08:25 -0500

    finished homework

--- events/myScript.js
+++ events/myScript.js
@@ -4,7 +4,7 @@ let number = 0;
 function myFunction() {
     let name = document.getElementById('name').value
     let animal = document.getElementById('animal').value
-    console.log('hello ' + name + '. Dont you wish you were a ' + animal + '?')
+    document.getElementById('message').innerHTML=('hello ' + name + '. Dont you wish you were a ' + animal + '?')
 }
 
 function changeColor() {
