9757480e2581976631a0c7c41beef9255b2ff046
anash24 <anash24@penguinhall.org>
2022-11-17 22:06:32 -0500

    homework

---
+++ events/myScript.js
@@ -0,0 +1,14 @@
+let number = 0;
+
+
+function myFunction() {
+    let name = document.getElementById('name').value
+    let animal = document.getElementById('animal').value
+    console.log('hello ' + name + '. Dont you wish you were a ' + animal + '?')
+}
+
+function changeColor() {
+    let name = document.getElementById('name').style.backgroundColor = "red"
+    
+}
+
